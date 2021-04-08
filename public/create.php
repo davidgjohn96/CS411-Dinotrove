57fc9365d84f03075a0d6534015b923f4da0ae00
David John <davidj96@live.unc.edu>
2021-04-08 18:54:37 -0400

    Add files

---
+++ public/create.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Dinotrove</title>
+
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+  
+<form method="post">
+	<label for="firstname">First Name</label>
+	<input type="text" name="firstname" id="firstname">
+	<label for="lastname">Last Name</label>
+	<input type="text" name="lastname" id="lastname">
+	<label for="userID">User ID</label>
+	<input type="text" name="userID" id="userID">
+	<label for="age">Age</label>
+	<input type="text" name="age" id="age">
+	<input type="submit" name="submit" value="Submit">
+</form>
+
+<a href="index.php">Back to home</a>
+
+
+  </body>
+</html>
---
+++ public/install.php
@@ -0,0 +1,1 @@
+$connection = new PDO(data source name, username, password, options);
