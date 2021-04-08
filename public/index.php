3524f7dba0f20a11c3414bc4baabd5d6fca088b6
David John <davidj96@live.unc.edu>
2021-04-08 18:50:19 -0400

    Add code

--- public/index.php
+++ public/index.php
@@ -5,20 +5,17 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>Simple Database App</title>
+    <title>Dinotrove</title>
 
     <link rel="stylesheet" href="css/style.css" />
   </head>
 
   <body>
-    <h1>Simple Database App</h1>
+    <h1>Dinotrove</h1>
 
     <ul>
       <li>
         <a href="create.php"><strong>Create</strong></a> - add a user
-      </li>
-      <li>
-        <a href="read.php"><strong>Read</strong></a> - find a user
       </li>
     </ul>
   </body>
